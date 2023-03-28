a91c3e27823dd9a1a5f83c7dd396543522b4d333
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-03-28 19:12:59 +0200

    create new todo form component

---
+++ src/features/todos/TodoForm/TodoForm.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../todosSlice';
+
+const TodoForm = () => {
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='task'>Task:</label>
+      <input
+        type='text'
+        name='task'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type='submit'>Add a ToDo</button>
+    </form>
+  );
+};
+
+export default TodoForm;
