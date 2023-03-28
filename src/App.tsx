ddade62769bd46cc9ce56a59816979a5e70bea2b
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-03-28 19:48:04 +0200

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import './App.css';
+import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <>
+      <nav>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/todos'>Your Todos</Link>
+        <br />
+        <Link to='/todos/new'>Add Todo</Link>
+      </nav>
+      <h1>Things ToDo!</h1>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,33 @@ import App from './App';
 import store from './store';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import TodoList from './features/todos/TodoList/TodoList';
+import TodoForm from './features/todos/TodoForm/TodoForm';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'todos',
+        element: <TodoList />,
+      },
+      {
+        path: 'todos/new',
+        element: <TodoForm />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
