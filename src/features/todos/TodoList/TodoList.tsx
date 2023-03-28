7d978a2b4af50101b585130dccea35affd74886e
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-03-28 19:12:36 +0200

    create todo list component

---
+++ src/features/todos/TodoList/TodoList.tsx
@@ -0,0 +1,12 @@
+import Todo from '../Todo/Todo';
+import { useSelector } from 'react-redux';
+import { selectTodos } from '../todosSlice';
+
+const TodoList = () => {
+  const todos = useSelector(selectTodos);
+  const todoElements = todos.map((todo) => <Todo key={todo.id} {...todo} />);
+
+  return <ul>{todoElements}</ul>;
+};
+
+export default TodoList;
